e4d011fd087e9721667201fd79891b3917de44f6
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-08-14 14:58:35 +0200

    Install packages for REST API

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(morgan());
+
+app.get('/', function(req, res){
+    res.send("Hello planet!");
+});
+
+app.listen(3000, function(){
+    console.log("Server listen on port*:" + port);
+});
