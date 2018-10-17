ba325d55c710e6c2ec8de39ecb2a7be7b35d5731
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-10-17 19:39:39 +0200

    Fix bugs auth.listener

--- src/events/listeners/auth.listener.js
+++ src/events/listeners/auth.listener.js
@@ -2,13 +2,13 @@ import UserServices from "../../services/user.services";
 
 export function AuthListener(req, res, next) {
     if (req.body.user) {
-        const user = UserServices.getUserByUsername(username);
+        const user = UserServices.getUserByUsername(req.body.username);
         if (user === req.body.user) {
             next();
         } else {
-            res.status(401).send(err);
+            res.status(401).send({message: 'Unauthorized'});
         }
     } else {
-        res.status(401).send(err);
+        res.status(401).send({message: 'Unauthorized'});
     }
 }
